9c420dd3754a4e619224ab33e7f19c449b8183ee
BigBlueHat <byoung@bigbluehat.com>
2016-04-04 16:59:13 -0400

    Add single doc HAL variant

---
+++ hal/shows/doc.js
@@ -0,0 +1,32 @@
+function(doc, req) {
+  // TODO: do something good if doc is empty
+
+  // Register the "hal" key with the "application/hal+json" content type
+  registerType("hal", "application/hal+json");
+  // Handle a request for the "hal" content type
+  provides("hal", function() {
+    var hal = doc;
+    hal._links = {
+      "self": {
+        "href": "/" + doc._id,
+        "title": doc.title || doc._id
+      },
+      "container": {
+        "href": "/",
+        "title": req.info.db_name
+      }
+    };
+
+    // Return the response
+    return {
+      "headers": {
+        // Tell the client we are responding with the "application/hal+json" content type
+        "Content-Type": "application/hal+json",
+        // Instruct the client to vary caching based on the "Accept" HTTP request header
+        "Vary": "Accept"
+      },
+      // Set the JSON object, seralized as a string, as the response body
+      "body": JSON.stringify(hal)
+    };
+  });
+}
